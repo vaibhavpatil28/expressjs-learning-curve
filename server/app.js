3f1a65f84b444af22b6f15a3258d2a6f69752f0b
Vaibhav Patil <developer.vaibhavpatil@gmail.com>
2020-04-03 13:35:26 +0530

    bable tarspiler is used for complie es6 and above code into es5

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path,{join} from 'path';
+
+// const express = require('express');
+// const path = require('path');
+
+const app = express();
+// const __dirname = join(process.cwd());
+// console.log('__dirname: ', __dirname);
+// app.use('public',express.static(path.join(__dirname, '../public')));
+// app.get('*.*', express.static(STATIC_FOLDER, {
+//     maxAge: '1y'
+// }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+app.get('/example/:name/:age', (req, res) => {
+    res.send(`Name: ${req.params.name}; Age : ${req.params.age}; tutorial : ${req.query.tutorial}; Sort By : ${req.query.sortBy}`);
+});
+
+// app.listen(3000);
+
+export default app;
