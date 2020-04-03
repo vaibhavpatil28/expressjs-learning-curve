7827d69ea73560ad72bcb4f9493f2b7546cd17ca
Vaibhav Patil <developer.vaibhavpatil@gmail.com>
2020-04-03 14:16:46 +0530

    serving static files without compiling code to es5

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ import path,{join} from 'path';
 // const path = require('path');
 
 const app = express();
-// const __dirname = join(process.cwd());
+const __dirname = join(process.cwd());
 // console.log('__dirname: ', __dirname);
-// app.use('public',express.static(path.join(__dirname, '../public')));
-// app.get('*.*', express.static(STATIC_FOLDER, {
-//     maxAge: '1y'
-// }));
-
-app.use(express.static(path.join(__dirname, '../public')));
-
+app.use('public',express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 app.get('/example/:name/:age', (req, res) => {
     res.send(`Name: ${req.params.name}; Age : ${req.params.age}; tutorial : ${req.query.tutorial}; Sort By : ${req.query.sortBy}`);
 });
 
-// app.listen(3000);
+app.listen(3000);
 
 export default app;
